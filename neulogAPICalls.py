f5f2d60e8709fc8ce425e9ca06100061cfd8bad5
SageRiverLee <69176656+SageRiverLee@users.noreply.github.com>
2024-02-14 19:23:32 -0800

    Last changes not saved.

--- neulogAPICalls.py
+++ neulogAPICalls.py
@@ -1,7 +1,12 @@
 import requests
 import time
-r = requests.get("http://localhost:22004/NeuLogAPI?GetServerVersion")
-print(r.json)
+print('Verifying API Connection')
+r = requests.get("http://localhost:22004/NeuLogAPI?GetServerStatus")
+if(r.json['GetServerStatus'] != "Ready"):
+    raise Exception("Neulog API not running") 
+print("Server status: ", r.json['GetServerStatus'])
+
+input("Calibrating Strength... Squeeze Hand Dynamometer at max strength (press any key to begin)")
 # while(True):
 #     start = time.time()
 #     r = requests.get("http://localhost:22004/NeuLogAPI?GetSensorValue:[HandDynamometer],[1]")
@@ -10,3 +15,11 @@ print(r.json)
 #     print(r.json())
 
 #     print("Seconds Elapsed: ", end - start)
+"""
+    Get the baseline strongest grip for calibration (how long to test for?) - likely set up as an experiment
+    Calcualte 20(or is it 30)% of their max strength 
+    Start experiment, tell user to start squeezing several times for (2 minutes?? get the amount of time)
+    How do we test it though
+    I would really like to have the research paper that he's talking about
+
+"""
